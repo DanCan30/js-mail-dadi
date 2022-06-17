230e712d0d296cf0ac417f3e3c6f69aaf17c440b
DanCan30 <danielecan3008@gmail.com>
2022-06-17 15:55:51 +0200

    Members validation

---
+++ script/main.js
@@ -0,0 +1,49 @@
+
+// Creating an array of mails
+const memberList = [
+    "user1@gmail.com", 
+    "user2@gmail.com", 
+    "user3@gmail.com", 
+    "user4@gmail.com", 
+    "user5@gmail.com", 
+    "user6@gmail.com", 
+    "user7@gmail.com", 
+    "user8@gmail.com", 
+    "user9@gmail.com", 
+    "user10@gmail.com", 
+    "user11@gmail.com", 
+    "user12@gmail.com", 
+    "user13@gmail.com", 
+    "user14@gmail.com", 
+    "user15@gmail.com"
+];
+
+// Creating a variable for the input
+let userInput;
+let member;
+let memberCheck = false;
+let logged = "Congratulation, You are a member."
+let error = "We are sorry, You are not a member, please sign."
+
+document.getElementById("submit").addEventListener("click", function() {
+
+    // Saving the user input in a variable
+    userInput = document.getElementById("user-email-input").value;
+
+    // Checking if the user input is in the memberList array
+    for ( let i = 0; i < memberList.length ; i++) {
+
+        member = memberList[i];
+
+        if (userInput === member) {
+            memberCheck = true;
+        }
+    }
+
+    // Depending on the check value a different message will appear on screen
+    if (memberCheck == true) {
+        document.getElementById("message").innerHTML = logged;
+    } else {
+        document.getElementById("message").innerHTML = error;
+    }
+});
